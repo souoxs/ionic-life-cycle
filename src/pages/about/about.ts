8a2be1a3d78a85c9b83fda7cc10b2c6ad99da43a
lilei <lilei@jiedaibao.com>
2018-03-20 15:08:17 +0800

    add home component

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-about',
@@ -9,6 +11,15 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goPage(url) {
+    if (url === 'home') {
+      this.navCtrl.push(HomePage);
+    }
+    else if (url === 'contact') {
+      this.navCtrl.push(ContactPage);
+    }
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ContactPage } from '../contact/contact';
+import { AboutPage } from '../about/about';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +12,15 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goPage(url) {
+    if (url === 'contact') {
+      this.navCtrl.push(ContactPage);
+    }
+    else if (url === 'about') {
+      this.navCtrl.push(AboutPage);
+    }
   }
 
 }
